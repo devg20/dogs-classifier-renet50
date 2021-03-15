f0a7687eef92cca13bb66f963f9b301ce64122b0
@dev.guha2000 <dev.guha2000@gmail.com>
2021-03-16 02:22:48 +0530

    removed opencv

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+"""
+Name: Dev Guha
+
+I tried deploying the api on heroku free tier, but the project size was turning out to be greated then 500mb
+which is not allowed on heroku free tier. So here is the code.
+"""
+
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS, cross_origin
 import json
+import pandas as pd
 import os
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import base64
 import numpy as np
 import io
 import tensorflow as tf
-import cv2
+from PIL import Image
 tf.compat.v1.disable_eager_execution()
 app = Flask(__name__)
 cors = CORS(app)
@@ -19,17 +28,16 @@ graph = tf.compat.v1.get_default_graph()
 
 @app.route('/api', methods=['POST'])
 def get_dog_breed():
+   size = (224,224)
    model = tf.keras.models.load_model("dogs_clf_v5_30.h5")
    graph = tf.compat.v1.get_default_graph()
    data = request.get_json()
    b64 = data["image"]
    dog_img = base64.b64decode(b64)
-   with open("dog_api.png", "wb") as fh:
-         fh.write(dog_img)
-   dog_img = cv2.imread("dog.png")
-   dog_img = cv2.resize(dog_img, (224,224))
+   dog_img = Image.open(io.BytesIO(dog_img))
+   dog_img = dog_img.resize(size)
+   dog_img = np.array(dog_img)
    dog_img = np.array([dog_img])
-   #print(dog_img.shape)
    with graph.as_default():
       res = model.predict(dog_img)
    res = list(res[0])
