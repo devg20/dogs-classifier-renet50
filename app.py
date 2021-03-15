22db5f4c467fd3cef4cce4e39ce833273e7b1479
@dev.guha2000 <dev.guha2000@gmail.com>
2021-03-16 03:04:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import base64
 import numpy as np
 import io
 import tensorflow as tf
+from tf.keras.applications.resnet50 import ResNet50
 from PIL import Image
 tf.compat.v1.disable_eager_execution()
 app = Flask(__name__)
